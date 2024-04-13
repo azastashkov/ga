551ef0fc269bf000e925e05a6422b7044a03fc1c
azastashkov <zastashkov@yahoo.com>
2024-04-13 20:17:50 +0300

    binary search

--- src/test/java/ch1/BinarySearchTest.java
+++ src/test/java/ch1/BinarySearchTest.java
@@ -1,5 +1,18 @@
 package ch1;
 
-public class BinarySearchTest {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class BinarySearchTest {
+    @Test
+    public void testBinarySearch() {
+        int[] array = { 1, 3, 5, 7, 9};
+        BinarySearch binarySearch = new BinarySearch();
+        assertEquals(1, binarySearch.binarySearch(array, 3));
+        assertEquals(4, binarySearch.binarySearch(array, 9));
+        assertEquals(-1, binarySearch.binarySearch(array, 4));
+        assertEquals(-1, binarySearch.binarySearch(array, 0));
+        assertEquals(-1, binarySearch.binarySearch(array, 10));
+    }
 }
